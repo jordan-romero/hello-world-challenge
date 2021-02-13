c86b500116ba336873cd61475da27f9321395de1
jordles113 <jordan.t.romero@gmail.com>
2021-02-13 13:49:18 -0600

    wires up ProjectHeader with ProjectGroups

---
+++ src/components/ProjectHeader.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ProjectHeader = () => {
+    return (
+        <div>
+            I AM A PROJECT HEADER
+        </div>
+    )
+}
+
+export default ProjectHeader
--- src/containers/ProjectGroups.js
+++ src/containers/ProjectGroups.js
@@ -1,9 +1,11 @@
 import React from 'react'
 import Projects from './Projects'
+import ProjectHeader from '../components/ProjectHeader'
 
 const ProjectGroups = () => {
     return (
         <div>
+           <ProjectHeader />
            <Projects /> 
         </div>
     )
