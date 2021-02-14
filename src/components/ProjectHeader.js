91ef1f96d9c6ecaf8ebe0f4e20c19894a4142cd6
jordles113 <jordan.t.romero@gmail.com>
2021-02-13 19:00:18 -0600

    completes typography styling, still need to work out lighter hues for some headings

--- src/components/ProjectHeader.js
+++ src/components/ProjectHeader.js
@@ -6,7 +6,7 @@ const ProjectHeader = (props) => {
     return (
         <div>
             <h1 className='heading-1'>{props.type} Projects</h1>
-            {props.type  === 'independent' ? <span className='heading-2 heading-2--blue'>Complete {props.group.required} of {props.group.projects.length}</span> : props.type}
+            {props.type  === 'independent' ? <span className='heading-2 heading-2--blue'>Complete {props.group.required} of {props.group.projects.length}</span> : null}
             <h2 className='heading-2'>{props.group.title}</h2>
         </div>
     )
